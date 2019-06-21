60d590c203c6f838e0b886687ec47db6257389d4
Phosphorescent Stars <46270670+PhosphorescentStars@users.noreply.github.com>
2019-06-20 21:58:06 -0300

    Adding a ViewModel to pass complex objects to the view

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,19 @@ namespace Vidly.Controllers
 		public ActionResult Random()
 		{
 			var movie = new Movie() { Name = "Shrek!" };
-			return View(movie);
+			var customers = new List<Customer>()
+			{
+				new Customer(){Name = "Customer 1" },
+				new Customer(){Name = "Customer 2" }
+			};
+
+			var viewModel = new RandomMovieViewModel()
+			{
+				Customers = customers,
+				Movie = movie
+			};
+
+			return View(viewModel);
 		}
 
 		public ActionResult Edit(int id)
@@ -32,7 +45,7 @@ namespace Vidly.Controllers
 			return Content($"PageIndex = {pageIndex}, sortBy = {sortBy}");
 		}
 
-		[Route("movies/released/{year:min(2000)}/{month:regex(\\d{2}):range(1,12)}") ]
+		[Route("movies/released/{year:min(2000)}/{month:regex(\\d{2}):range(1,12)}")]
 		public ActionResult ByReleaseDate(int year, int month)
 		{
 			return Content($"Resultado: {month}/{year}");
